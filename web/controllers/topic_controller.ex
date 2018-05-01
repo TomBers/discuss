9347d2386bbd99ab947e77967214f86fa4bbc9ba
TomBers <tjcberman@gmail.com>
2018-05-01 15:24:07 +0100

    Discus project

---
+++ web/controllers/topic_controller.ex
@@ -0,0 +1,10 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+
+  alias Discuss.Topic
+
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
+  end
+end
