2fea35ea86f60c5039222416adaaed2c07ce7fa5
TomBers <tjcberman@gmail.com>
2018-05-01 16:47:11 +0100

    Add databse operations

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -3,8 +3,25 @@ defmodule Discuss.TopicController do
 
   alias Discuss.Topic
 
+  def index(conn, _params) do
+    query = from topic in Topic,
+      select: topic.title
+    IO.inspect(Repo.all(query))
+    render conn, "index.html", topics: Repo.all(query)
+  end
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+    case Repo.insert(changeset) do
+      {:ok, post} ->
+        render conn, "success.html"
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
